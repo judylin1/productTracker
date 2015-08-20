bc2971b711fa34ac06fcbc55eb7bdce8bb707829
Judy Lin <judith.s.lin@gmail.com>
2015-08-20 10:43:09 -0600

    heroku deploy

--- app.js
+++ app.js
@@ -72,6 +72,11 @@ if (app.get('env') === 'development') {
   });
 }
 
+// heroku 
+if (app.get('env') === 'production') {
+  app.listen(3000);
+}
+
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
